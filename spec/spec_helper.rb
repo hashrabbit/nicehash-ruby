bd42d0525a2d4d9d0434bf1382fc91f8cc0ae50e
Brian V. Hughes <brianvh89@gmail.com>
2020-04-04 13:38:29 -0700

    Add awesome_print and dotenv support to spec_helper
    Remove placeholder nicehash_spec file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,30 @@
 require 'bundler/setup'
+require 'awesome_print'
+require 'dotenv'
+Dotenv.load
 require 'nicehash'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
+  config.example_status_persistence_file_path = ".rspec_status"
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
   config.disable_monkey_patching!
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
